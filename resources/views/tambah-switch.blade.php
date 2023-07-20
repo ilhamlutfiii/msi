82884328e76c930273d064d6d5451ec0c75e6307
= <=>
2023-07-20 14:33:15 +0700

    add switch

--- resources/views/tambah-switch.blade.php
+++ resources/views/tambah-switch.blade.php
@@ -36,17 +36,17 @@
 
             <div class="form-group"><label class="form-control-label">Port :</label><input type="number" name="port" class="form-control" ></div>
 
-            <div class="form-group"><label class="form-control-label">Nama :</label><input type="number" name="nama" class="form-control" ></div>
+            <div class="form-group"><label class="form-control-label">Nama :</label><input type="text" name="nama" class="form-control" ></div>
 
-            <div class="form-group"><label class="form-control-label">Tipe :</label><input type="number" name="tipe" class="form-control" ></div>
+            <div class="form-group"><label class="form-control-label">Tipe :</label><input type="text" name="tipe" class="form-control" ></div>
 
-            <div class="form-group"><label class="form-control-label">SN :</label><input type="number" name="sn" class="form-control"></div>
+            <div class="form-group"><label class="form-control-label">SN :</label><input type="text" name="sn" class="form-control"></div>
 
-            <div class="form-group"><label class="form-control-label">Letak :</label><input type="number" name="letak" class="form-control" ></div>
+            <div class="form-group"><label class="form-control-label">Letak :</label><input type="text" name="letak" class="form-control" ></div>
 		
-            <div class="form-group"><label class="form-control-label">Mac :</label><input type="number" name="mac" class="form-control" ></div>
+            <div class="form-group"><label class="form-control-label">Mac :</label><input type="text" name="mac" class="form-control" ></div>
 
-            <div class="form-group"><label class="form-control-label">Macc :</label><input type="number" name="macc" class="form-control" ></div>
+            <div class="form-group"><label class="form-control-label">Macc :</label><input type="text" name="macc" class="form-control" ></div>
 
 		    <div class="form-group"><label class="form-control-label">IP address :</label>
 			    <select name="ip_id" id="select" class="form-control">
@@ -57,7 +57,7 @@
 			    </select>
 		    </div>
 
-            <div class="form-group"><label class="form-control-label">Referensi :</label><input type="number" name="referensi" class="form-control"></div>
+            <div class="form-group"><label class="form-control-label">Referensi :</label><input type="text" name="referensi" class="form-control"></div>
 
 			<button type="submit" class="btn btn-primary btn-sm">
 				<i class="fa fa-dot-circle-o"></i> Submit
